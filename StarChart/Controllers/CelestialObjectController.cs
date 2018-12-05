06815d2760e349f6829ec96e0db4962e68f571dc
idristhabet <thabet.idris@hotmail.fr>
2018-12-05 20:56:48 +0100

    GetById

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,7 +18,7 @@ namespace StarChart.Controllers
             _context = context;
         }
 
-        [HttpGet("{id:id}",Name="GetById")]
+        [HttpGet("{id:int}",Name="GetById")]
         public IActionResult GetById(int id)
         {
             var celestial = _context.CelestialObjects.Find(id);
